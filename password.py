cf3a150d2b4db5bf7067c2fa0593e6efdd9899d4
7382022 <amreenrida2@gmail.com>
2024-12-16 00:30:38 -0500

    password

---
+++ password.py
@@ -0,0 +1,7 @@
+import random
+import time
+
+number=random.randint(1,1000)
+Uppercase=random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+Lowercase=random.choice('abcdefghijklmnopqrstuvwxyz')
+print(number,Uppercase,Lowercase)
